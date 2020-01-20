5aa612365ee70af2acf310aa79b6c0751bc4224e
Monwabisi Dingane <50623488+i-am-monza@users.noreply.github.com>
2020-01-21 00:17:44 +0200

    Updates

---
+++ precache-manifest.021ecb04de697af141c9794cafd97a5a.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "634bca6ab0a0f5846964e01eec75ea4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9262fd363db33c52f1e7",
+    "url": "/static/css/main.ca1e1a1d.chunk.css"
+  },
+  {
+    "revision": "4aab209150f97b4bc9da",
+    "url": "/static/js/2.e72e2981.chunk.js"
+  },
+  {
+    "revision": "9262fd363db33c52f1e7",
+    "url": "/static/js/main.c55ba67c.chunk.js"
+  },
+  {
+    "revision": "02e335ffdba951906b9c",
+    "url": "/static/js/runtime-main.06ed6d10.js"
+  },
+  {
+    "revision": "315df6b3b396d4d5de0856efd19e374b",
+    "url": "/static/media/01.315df6b3.jpg"
+  },
+  {
+    "revision": "0513869c63f0601b0e3f43e77e1faaf7",
+    "url": "/static/media/02.0513869c.jpg"
+  },
+  {
+    "revision": "1a72066ec239017b08bbf795a02ae397",
+    "url": "/static/media/02.1a72066e.jpg"
+  },
+  {
+    "revision": "bc36372255e42f16969cc710448472a1",
+    "url": "/static/media/03.bc363722.jpg"
+  },
+  {
+    "revision": "d3e8db3d881d70e37148a952884986b2",
+    "url": "/static/media/03.d3e8db3d.jpg"
+  },
+  {
+    "revision": "84d8c023d4e127cf95755e1ea3b7900c",
+    "url": "/static/media/04.84d8c023.jpg"
+  },
+  {
+    "revision": "cbb4069b7f71985d8a4f940dc4e60977",
+    "url": "/static/media/05.cbb4069b.jpg"
+  },
+  {
+    "revision": "3205b2d136d00f03bda659a3e28cef60",
+    "url": "/static/media/06.3205b2d1.jpg"
+  },
+  {
+    "revision": "a38b47272314e9a0308026d0dee53e85",
+    "url": "/static/media/07.a38b4727.jpg"
+  },
+  {
+    "revision": "0bb276fefad047f5960c770dacac9b7b",
+    "url": "/static/media/08.0bb276fe.jpg"
+  },
+  {
+    "revision": "9b6a0453a957034480ec44d463893b32",
+    "url": "/static/media/3.9b6a0453.jpg"
+  },
+  {
+    "revision": "31741762565742c1fdcbe52154ee3b5f",
+    "url": "/static/media/blax.31741762.jpg"
+  },
+  {
+    "revision": "84e16c6efe1894745b9ee3699dd426a5",
+    "url": "/static/media/catering.84e16c6e.jpg"
+  },
+  {
+    "revision": "c94661b5c8b3e3b7233a908df06e8a71",
+    "url": "/static/media/contact.c94661b5.jpg"
+  },
+  {
+    "revision": "d8126dfd24b77a8b75bd1dbe2f01d875",
+    "url": "/static/media/estratweni-main-image.d8126dfd.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "29e1c834e6b16a5ad6e5a49f2f547ac1",
+    "url": "/static/media/floor-director.29e1c834.jpg"
+  },
+  {
+    "revision": "139e74e298bca37a25d2bd5868552e04",
+    "url": "/static/media/fontawesome-webfont.139e74e2.svg"
+  },
+  {
+    "revision": "706450d7bba6374ca02fe167d86685cb",
+    "url": "/static/media/fontawesome-webfont.706450d7.ttf"
+  },
+  {
+    "revision": "97493d3f11c0a3bd5cbd959f5d19b699",
+    "url": "/static/media/fontawesome-webfont.97493d3f.woff2"
+  },
+  {
+    "revision": "d9ee23d59d0e0e727b51368b458a0bff",
+    "url": "/static/media/fontawesome-webfont.d9ee23d5.woff"
+  },
+  {
+    "revision": "f7c2b4b747b1a225eb8dee034134a1b0",
+    "url": "/static/media/fontawesome-webfont.f7c2b4b7.eot"
+  },
+  {
+    "revision": "63f384bf2b0c8e9d4c28d2de88bcb1f7",
+    "url": "/static/media/help.63f384bf.pdf"
+  },
+  {
+    "revision": "5c7af4f605f60cfa107b010c4be0aeee",
+    "url": "/static/media/hiring.5c7af4f6.jpg"
+  },
+  {
+    "revision": "779fd1ceddd28b7a17bdfca9adbedc05",
+    "url": "/static/media/kitchen-manager.779fd1ce.jpg"
+  },
+  {
+    "revision": "0abfb99af9fe34a8d4ddc5ef57fd270e",
+    "url": "/static/media/menu-image5.0abfb99a.jpg"
+  },
+  {
+    "revision": "363872d1479bc62957548ee30859fa7f",
+    "url": "/static/media/space.363872d1.jpg"
+  },
+  {
+    "revision": "54e142cf4dff9e029a51b87bfe2cb580",
+    "url": "/static/media/subscribe.54e142cf.jpg"
+  }
+]);
